a4806b12d29b278d2bc47d939003a952d90ffb09
nins-web <goceng000@gmail.com>
2026-09-07 12:14:48 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-tsx
 import './globals.css'
 export default function RootLayout({children}:{children:React.ReactNode}){
   return <html lang="id"><body className="bg-zinc-950 text-white min-h-screen">{children}</body></html>
