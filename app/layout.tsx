8d666deeeb0ec7b39dddef064b1a6030d408e4cb
nins-web <goceng000@gmail.com>
2026-09-07 16:25:38 +0700

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-import './globals.css'
-export default function RootLayout({children}:{children:React.ReactNode}){
-  return <html lang="id"><body className="bg-zinc-950 text-white min-h-screen">{children}</body></html>
-}
